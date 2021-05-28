5dfbf1f4662458557364718527cf392bdaf45e40
chenjiefun <cj781099725>
2021-05-28 13:56:27 +0800

    数据解析99

--- src/test/java/com/lemon/day07/TestNGDem.java
+++ src/test/java/com/lemon/day07/TestNGDem.java
@@ -1,5 +1,16 @@
 package com.lemon.day07;
 
+/**
+ * @Test;用来标记测试方法
+ * 测试方法执行顺序，默认是按照方法名的字典顺序来的
+ *
+ * Alt + Shift + 向上/下箭头： 移动一行
+ * Ctrl + Shift + 向上/下箭头：移动整个方法
+ *
+ * priority;指定优先级，赋值一个整数，数值越大，优先级越低
+ *
+ */
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,12 +22,12 @@ public class TestNGDem {
 //        Assert.assertEquals(au,5);
 //    }
     @Test
-    public void a(){
-        System.out.println("TestNGDem.a()");
-    }
-    @Test
     public void b(){
         System.out.println("TestNGDem.b()");
+    }
+    @Test
+    public void a(){
+        System.out.println("TestNGDem.a()");
     }
     @Test
     public void c(){
